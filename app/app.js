85cab470fb5cc8d07cf0c7b6bd07820231396032
Jacob Milligan <jacobpmilligan@gmail.com>
2015-12-14 17:37:56 +1100

    Separate routes and controllers

--- app/app.js
+++ app/app.js
@@ -1,16 +1,16 @@
 'use strict';
 
 var express = require('express');
+var app = express();
+
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
+// Get defined routes
+var routes = require('./config/router')(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,9 +23,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ app/config/router.js
@@ -0,0 +1,8 @@
+'use strict';
+var index = require('../controllers/index');
+var users = require('../controllers/users');
+
+module.exports = function(app) {
+	app.use('/', index);
+	app.use('/users', users);
+};
