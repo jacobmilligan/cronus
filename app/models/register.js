661c53cc3726dcac048ea34d8fd9466d9ba77371
Jacob Milligan <jacobpmilligan@gmail.com>
2015-12-15 16:57:49 +1100

    Check for presence of input fields before inserting

--- app/models/register.js
+++ app/models/register.js
@@ -5,9 +5,18 @@ var db = require('./db');
 function createUser(postBody, callback) {
 	var body = [];
 	var errStr = "";
+	// make body into array
 	for ( var key in postBody ) {
+		if ( postBody[key].length <= 0 ) {
+			errStr += key + " is required.\n";
+		}
 		body.push(postBody[key]);
 	}
+
+	if ( errStr.length > 0 ) {
+		return callback(null, errStr);
+	}
+
 	var sql = "INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)";
 	db.query(sql, body, function(err, result) {
 		if (err) {
