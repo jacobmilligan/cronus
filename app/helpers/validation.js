c83ec5e42507fb2198959e610f328d4a6ca95f97
Jacob Milligan <jacobpmilligan@gmail.com>
2015-12-20 17:37:36 +1100

    Add validation

---
+++ app/helpers/validation.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var joi = require('joi');
+
+// Setup validation schemas
+var registerSchema = {
+	_csrf: joi.string(),
+	email: joi.string().email(),
+  	password: joi.string().min(6),
+  	first_name: joi.string().required(),
+  	last_name: joi.string()
+};
+
+function register(val) {
+	var errStr = "";
+	joi.validate(val, registerSchema, function(err, result) {
+		if (err) {
+			errStr = err.details[0].message;
+			switch (err.details[0].path) {
+				case "email":
+					errStr = errStr.replace("\"email\"", "eMail");
+					break;
+				case "password":
+					errStr = errStr.replace("\"password\"", "Password") + " and must be at least 6 characters long";
+					break;
+				case "first_name":
+					errStr = errStr.replace("\"first_name\"", "First Name");
+					break;
+				case "last_name":
+					errStr = errStr.replace("\"last_name\"", "Last Name");
+					break;
+			}
+		} else {
+			return null;
+		}
+	});
+	return errStr;
+}
+
+module.exports = {
+	register: register
+};
