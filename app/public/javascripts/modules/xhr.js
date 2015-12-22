78fabb80f3f557ea029bb10739756b7d6909be93
Jacob Milligan <jacobpmilligan@gmail.com>
2015-12-22 19:19:54 +1100

    Everything inside IIFE

--- app/public/javascripts/modules/xhr.js
+++ app/public/javascripts/modules/xhr.js
@@ -7,31 +7,35 @@
 		getProjects();
 	}
 
-}());
+	function getProjects() {
+		var projects = document.getElementById('projects');
+			var noProjects = document.getElementById('no-projects');
 
-function getProjects() {
-	var projects = document.getElementById('projects');
-		var noProjects = document.getElementById('no-projects');
+			var projReq = new XMLHttpRequest();
 
-		var projReq = new XMLHttpRequest();
+			projReq.onreadystatechange = function() {
+				if ( projReq.readyState === 4 && projReq.status === 200 ) {
+					var projRes = JSON.parse(projReq.responseText);
 
-		projReq.onreadystatechange = function() {
-			if ( projReq.readyState === 4 && projReq.status === 200 ) {
-				var projRes = JSON.parse(projReq.responseText);
-
-				if ( projRes.length > 0 ) {
-					projects.style.display = 'block';
-					for (var i = 0; i < projRes.length; i++) {
-						projects.innerHTML += projRes[i].project_name;
-						projects.innerHTML += projRes[i].description;
+					if ( projRes.length > 0 ) {
+						projects.style.display = 'block';
+						for (var i = 0; i < projRes.length; i++) {
+							projects.innerHTML += projRes[i].project_name;
+							projects.innerHTML += projRes[i].description;
+						}
+					} else {
+						noProjects.style.display = 'block';
 					}
-				} else {
-					noProjects.style.display = 'block';
+
 				}
+			};
 
-			}
-		};
+			projReq.open('GET', 'projects');
+			projReq.send();
+	}
 
-		projReq.open('GET', 'projects');
-		projReq.send();
-}
+	function buildProject() {
+		
+	}
+
+}());
