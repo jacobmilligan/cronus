24df15d5bd63a148efd4ff6e9ac582746e2abb5c
Jacob Milligan <jacobpmilligan@gmail.com>
2015-12-22 19:18:08 +1100

    Seperate functions

--- app/public/javascripts/modules/xhr.js
+++ app/public/javascripts/modules/xhr.js
@@ -3,7 +3,14 @@
 (function() {
 
 	if ( window.location.href.indexOf('dashboard') > -1 ) {
-		var projects = document.getElementById('projects');
+
+		getProjects();
+	}
+
+}());
+
+function getProjects() {
+	var projects = document.getElementById('projects');
 		var noProjects = document.getElementById('no-projects');
 
 		var projReq = new XMLHttpRequest();
@@ -11,13 +18,20 @@
 		projReq.onreadystatechange = function() {
 			if ( projReq.readyState === 4 && projReq.status === 200 ) {
 				var projRes = JSON.parse(projReq.responseText);
-				projects.style.display = 'block';
-				projects.innerHTML = projRes;
+
+				if ( projRes.length > 0 ) {
+					projects.style.display = 'block';
+					for (var i = 0; i < projRes.length; i++) {
+						projects.innerHTML += projRes[i].project_name;
+						projects.innerHTML += projRes[i].description;
+					}
+				} else {
+					noProjects.style.display = 'block';
+				}
+
 			}
 		};
 
 		projReq.open('GET', 'projects');
 		projReq.send();
-	}
-
-}());
+}
