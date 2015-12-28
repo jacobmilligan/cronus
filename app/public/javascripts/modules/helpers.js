e7f76c4cebad1a8c0244f50ed0ad470c8c3c04c0
Jacob Milligan <jacobpmilligan@gmail.com>
2015-12-28 14:42:43 +1100

    Edit helpers file

--- app/public/javascripts/modules/helpers.js
+++ app/public/javascripts/modules/helpers.js
@@ -14,7 +14,7 @@ function detectTouch(element, event, add) {
 			element.removeEventListener('click', event);
 		}
 	}
-	
+
 }
 
 function getTransitionTime(element) {
@@ -25,18 +25,18 @@ function getTransitionTime(element) {
 
 function mixColor(base, color, weight) {
   function d2h(d) { return d.toString(16); }  // convert a decimal value to hex
-  function h2d(h) { return parseInt(h, 16); } // convert a hex value to decimal 
+  function h2d(h) { return parseInt(h, 16); } // convert a hex value to decimal
 
   var percentChange = (typeof(weight) !== 'undefined') ? weight : 50; // set the percent to 50%, if that argument is omitted
 
   var result = "#";
 
-  for(var i = 0; i <= 5; i += 2) { // loop through each of the 3 hex pairs—red, green, and blue
+  for(var i = 0; i <= 5; i += 2) { // loop through each of the 3 hex pairs— red, green, and blue
     var v1 = h2d(base.substr(i, 2)), // extract the current pairs
         v2 = h2d(color.substr(i, 2)),
 
         // combine the current pairs from each source color, according to the specified percent
-        val = d2h(Math.floor(v2 + (v1 - v2) * (percentChange / 100.0))); 
+        val = d2h(Math.floor(v2 + (v1 - v2) * (percentChange / 100.0)));
 
     while(val.length < 2) { val = '0' + val; } // prepend a '0' if val results in a single digit
 
