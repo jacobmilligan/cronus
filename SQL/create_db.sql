31c3882d310775b8ae70a3b37333f9f88eb4d40e
Jacob Milligan <jacobpmilligan@gmail.com>
2015-12-31 01:15:11 +1100

    Change DB SQL file

--- SQL/create_db.sql
+++ SQL/create_db.sql
@@ -25,7 +25,7 @@ value 			money,
 start_time		TIMESTAMP WITH TIME ZONE NOT NULL,
 end_time		TIMESTAMP WITH TIME ZONE,
 elapsed			interval DEFAULT '0 0:00:00' NOT NULL,
-PRIMARY KEY(task_name, project_name, user_id),
+PRIMARY KEY(task_name, project_name, user_id, start_time),
 FOREIGN KEY(project_name, user_id) REFERENCES projects (project_name, user_id)
 );
 
@@ -52,7 +52,8 @@ content			varchar(100),
 user_id			integer,
 project_name	varchar(125),
 task_name		varchar(125),
+start_time		TIMESTAMP WITH TIME ZONE NOT NULL,
 PRIMARY KEY(content, user_id, project_name, task_name),
 FOREIGN KEY(creator_id, content) REFERENCES tags (creator_id, content),
-FOREIGN KEY(user_id, project_name, task_name) REFERENCES tasks (user_id, project_name, task_name)
+FOREIGN KEY(user_id, project_name, task_name, start_time) REFERENCES tasks (user_id, project_name, task_name, start_time)
 );
